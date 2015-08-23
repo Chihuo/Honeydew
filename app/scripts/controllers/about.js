cabaaec60212f2cff455400c45d9f744fd426dc3
Roish <roy.lihui.shen@gmail.com>
2015-08-23 13:08:20 -0700

    fresh cook yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name honeydewApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the honeydewApp
+ */
+angular.module('honeydewApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name honeydewApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the honeydewApp
+ */
+angular.module('honeydewApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
